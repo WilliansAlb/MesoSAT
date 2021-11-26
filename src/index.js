1db7bfee15a6e1fb4a220becd50522d03266492c
Willians Orozco <williansalbertolpez@hotmail.com>
2021-11-26 00:12:37 -0600

    Prueba 3

--- src/index.js
+++ src/index.js
@@ -8,17 +8,10 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio.listen(server);
-
-mongoose.connect('mongodb+srv://davidmonzon:davidmonzon@mesosat.ykyr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-    .then(db => console.log('Base de datos conectada'))
-    .catch(err => console.log(err));
 
 app.set('port', process.env.PORT || 3000);
 
 
 app.use(express.static(path.join(__dirname, "public")));
 
-server.listen(app.get('port'), ()=>{
-    console.log('Server en el puerto '+app.get('port'));
-});
+server.listen(app.get('port'));
