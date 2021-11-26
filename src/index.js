3862671427f9bbc7a1a52976bbe97ea84dabf99f
Willians Orozco <williansalbertolpez@hotmail.com>
2021-11-26 01:04:35 -0600

    Prueba 10

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const express = require("express"),
+    path = require("path"),
+    app = express(),
+    puerto = 3000;
 
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-//set views
-app.set('views', './views');
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', (peticion, respuesta) => {
+    // Podemos acceder a la petición HTTP
+    let agenteDeUsuario = peticion.header("user-agent");
+    respuesta.send("La ruta / solicitada con: " + agenteDeUsuario);
 });
 
-app.get('/entrar', (req, res) => {
-    res.render('registro');
+app.get('/hola', (peticion, respuesta) => {
+    let mascota = {
+        nombre: "Maggie",
+        edad: 2,
+    };
+    respuesta.json(mascota);
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+// Una vez definidas nuestras rutas podemos iniciar el servidor
+app.listen(puerto, err => {
+    if (err) {
+        // Aquí manejar el error
+        console.error("Error escuchando: ", err);
+        return;
+    }
+    // Si no se detuvo arriba con el return, entonces todo va bien ;)
+    console.log(`Escuchando en el puerto :${puerto}`);
 });
-
-app.get('/faqs', (req, res) => {
-    res.render('faqs');
-});
-
-app.get('/about', (req, res) => {
-    res.status(201).sendFile(path.join(__dirname, '../views/about.html'));
-});
-
-app.use( (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '../views/404.html'));
-});
-
-app.listen(3000 || process.env.PORT, () => console.log('Escuchando en el puerto 3000'));
