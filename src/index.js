e33568f0b06d31783fe51ba585ed40d13e9ac165
Willians Orozco <williansalbertolpez@hotmail.com>
2021-11-26 12:05:48 -0600

    Prueba 21

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ const express = require("express"),
     path = require("path"),
     app = express(),
     puerto = process.env.PORT || 3000; // Si está definido en el entorno, usarlo. Si no, el 3000
+const router = express.Router();
+
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
-
-app.get('/another', (peticion, respuesta) => {
-    // Podemos acceder a la petición HTTP
-    let agenteDeUsuario = peticion.header("user-agent");
-    respuesta.send("La ruta / solicitada con: " + agenteDeUsuario);
-});
 
 app.get('/', (peticion, respuesta) => {
     // Servir archivo HTML, o cualquier otro archivo
@@ -20,18 +16,14 @@ app.get('/', (peticion, respuesta) => {
     respuesta.sendFile(rutaDeArchivo);
 });
 
+router.get('/prueba',(peticion,respuesta) =>{
+    respuesta.send("prueba 1");
+});
+
 app.get('/grafica', (peticion, respuesta) => {
     // Servir archivo HTML, o cualquier otro archivo
     let rutaDeArchivo = path.join(__dirname, "grafica.html");
     respuesta.sendFile(rutaDeArchivo);
-});
-
-app.get('/hola', (peticion, respuesta) => {
-    let mascota = {
-        nombre: "Maggie",
-        edad: 2,
-    };
-    respuesta.json(mascota);
 });
 
 // Una vez definidas nuestras rutas podemos iniciar el servidor
