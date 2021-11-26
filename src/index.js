397d9aa645f34e888e0a78d8fd7b1ba70c59fffd
Willians Orozco <williansalbertolpez@hotmail.com>
2021-11-26 00:39:24 -0600

    Prueba 5

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ const mongoose = require('mongoose');
 const http = require('http');
 const path = require('path');
 
-
 const app = express();
 const server = http.createServer(app);
 
+const url= 'mongodb+srv://davidmonzon:davidmonzon@mesosat.ykyr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+
+mongoose.connect(url,options).then(
+    ()=>{console.log('Conectado a mongo');},
+    err=>{err}
+);
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,'public')));
 
-server.listen(app.get('port'));
+require('./sockets')(io);
+
+server.listen(app.get('port',()=>{
+    console.log('server');
+}));
