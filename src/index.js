e608b60da06ffc7d6612d6e193adcc9b957d3ebc
Willians Orozco <williansalbertolpez@hotmail.com>
2021-11-26 00:19:21 -0600

    Prueba 5

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const server = http.createServer(app);
 
 app.set('port', process.env.PORT || 3000);
 
-
 app.use(express.static(path.join(__dirname, "public")));
 
 server.listen(app.get('port'));
