5cffec78639d422a30bea6080f92bb69cda7c046
Willians Orozco <williansalbertolpez@hotmail.com>
2021-11-27 00:26:13 -0600

    Prueba mysql

--- src/index.js
+++ src/index.js
@@ -1,150 +1,88 @@
-const express = require("express"),
-    path = require("path"),
-    app = express(),
-    puerto = process.env.PORT || 3000,
-    bodyParser = require('body-parser'); // Si está definido en el entorno, usarlo. Si no, el 3000
+const express = require("express");
+const mysql = require('mysql');
+const PORT = process.env.PORT || 3000;
+const app = express()
+
 app.use(express.json());
 
-let base;
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://davidmonzon:davidmonzon@mesosat.ykyr0.mongodb.net/Registro?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+const connection = mysql.createConnection({
+    host: 'buhv2nxcrazbcbhexwra-mysql.services.clever-cloud.com',
+    user: 'uu4r4dutxx92o6su',
+    password: 'rL6xundnlmNuUGUkdYBZ',
+    database: 'buhv2nxcrazbcbhexwra'
+});
+
+//Rutas
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API SAT')
 })
-    .then(db => {
-        base = db;
-        console.log("base de datos conectada");
-    })
-    .catch(err => console.log(err));
 
-const productSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    valor: Number
-});
+app.get('/leer', (req, res) => {
+    const sql = 'SELECT * FROM lectura';
 
-const reg = mongoose.model('Registro', productSchema);
-
-app.get("/insertar", (peticion, respuesta) => {
-    const product = new reg({
-        _id: new mongoose.Types.ObjectId(),
-        valor: peticion.query.reg
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('Sin resultados');
+        }
     });
-    if (peticion.query.reg) {
-        product
-            .save()
-            .then(result => {
-                console.log(result);
-                respuesta.status(201).json({
-                    message: "Handling POST requests to /insertar",
-                    createdProduct: result
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                respuesta.status(500).json({
-                    error: err
-                });
-            });
-    } else {
-        respuesta.status(500).json({
-            error: "No sé que error es"
-        });
-    }
 });
 
-app.get("/actualizar", (peticion, respuesta) => {
-    reg.findOne({
-        id: 0
-    })
-        .then((registro) => {
-            registro.valor = peticion.query.reg;
-            registro
-                .save()
-                .then(() => {
-                    respuesta.jsonp({ registro }); // enviamos la registro de vuelta
-                });
-        });
-});
+app.post('/guardar', (req, res) => {
+    const sql = 'INSERT INTO lectura SET ?';
 
-app.post("/actualizar", (peticion, respuesta) => {
-    reg.findOne({
-        id: 0
-    })
-        .then((registro) => {
-            registro.valor = peticion.body.reg;
-            registro
-                .save()
-                .then(() => {
-                    respuesta.jsonp({ registro }); // enviamos la registro de vuelta
-                });
-        });
-});
+    var today = new Date();
+    var year = today.getFullYear();
+    var mes = today.getMonth() + 1;
+    var dia = today.getDate();
+    var fecha = year + "-" + mes + "-" + dia;
 
-app.get("/obtener", (peticion, respuesta) => {
-    reg.findOne({
-        id: 0
-    })
-        .then((registro) => {
-            respuesta.json(registro.valor);
-        });
-});
-
-app.post("/insertar", (peticion, respuesta) => {
-    const product = new reg({
-        _id: new mongoose.Types.ObjectId(),
-        valor: peticion.body.reg,
-        id: 9
+    const lectura = {
+        valor: req.body.valor,
+        fecha: fecha,
+        hora: today.getHours()
+    };
+    connection.query(sql, lectura, error => {
+        if (error) throw error;
+        res.send('Lectura guardada');
     });
-    if (peticion.body.reg) {
-        product
-            .save()
-            .then(result => {
-                console.log(result);
-                respuesta.status(201).json({
-                    message: "Handling POST requests to /insertar",
-                    createdProduct: result
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                respuesta.status(500).json({
-                    error: err
-                });
-            });
-    } else {
-        respuesta.status(500).json({
-            error: "No sé que error es"
-        });
-    }
 });
 
-app.use(express.static('public'));
+app.post('/obtener', (req, res) => {
+    const sql = 'SELECT * FROM lectura WHERE fecha=?';
 
-app.use('/css', express.static(__dirname + 'public/css'));
+    const lectura = req.body.fecha;
 
-app.use('/js', express.static(__dirname + 'public/js'));
-
-app.use('/img', express.static(__dirname + 'public/img'));
-
-
-app.get('/', (peticion, respuesta) => {
-    // Servir archivo HTML, o cualquier otro archivo
-    let rutaDeArchivo = path.join(__dirname, "index.html");
-    respuesta.sendFile(rutaDeArchivo);
+    connection.query(sql, lectura, error => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('Sin resultados');
+        }
+    });
 });
 
-app.get('/grafica', (peticion, respuesta) => {
-    // Servir archivo HTML, o cualquier otro archivo
-    let rutaDeArchivo = path.join(__dirname, "grafica.html");
-    respuesta.sendFile(rutaDeArchivo);
+app.get('/ver/:fecha', (request, res) => {
+    let select = 'SELECT * FROM lectura WHERE fecha = ?'
+    connection.query(select, request.params.fecha, (error, result) => {
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('Sin resultados');
+        }
+    })
+})
+
+
+//Conexion
+connection.connect(error => {
+    if (error) throw error;
+    console.log("Conexion BD exitosa");
 });
 
-// Una vez definidas nuestras rutas podemos iniciar el servidor
-app.listen(puerto, err => {
-    if (err) {
-        // Aquí manejar el error
-        console.error("Error escuchando: ", err);
-        return;
-    }
-    // Si no se detuvo arriba con el return, entonces todo va bien ;)
-    console.log(`Escuchando en el puerto :${puerto}`);
-});
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto: ${PORT}`)
+})
